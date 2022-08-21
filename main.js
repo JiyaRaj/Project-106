5a97cdd8f4a4d33a8be5bafc4094f9f8589b4ad6
JiyaRaj <86504048+JiyaRaj@users.noreply.github.com>
2022-08-21 12:14:13 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 https://teachablemachine.withgoogle.com/models/nYqWI_It-/
+
+function Start(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/nYqWI_It-/model.json", modelready);
+
+}
+
+function modelready(){
+    classifier.classify(gotResults)
+}
+
+function gotResults(error,result){
+    console.log(result);
+}
